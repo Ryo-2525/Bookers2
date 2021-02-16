6cf4ff5e8307272e13e6fe0bdfb7b99fde9bf458
Ryo-2525 <seama.24.music@gmail.com>
2021-02-16 15:56:52 +0000

    [start]rspec

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,7 +18,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      flash[:notice] = "successfully posted!"
+      flash[:notice] = "You have created book successfully."
       redirect_to book_path(@book.id)
     else
       render("books/index")
@@ -32,7 +32,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      flash[:notice] = "successfully edited!"
+      flash[:notice] = "You have updated book successfully."
       redirect_to book_path(@book.id)
     else
       render("books/edit")
